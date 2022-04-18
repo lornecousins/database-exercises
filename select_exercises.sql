0d41353add330a3d93cbd071136e7395e057d368
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-18 17:02:37 -0500

    chore: Begin Basic Statements Exercise

---
+++ select_exercises.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+SELECT * From albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date FROM albums Where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums WHERE name = 'Nevermind';
