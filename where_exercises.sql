de9ae63e1191a17408abf3054820f6ad2f4db93b
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-19 15:27:52 -0500

    Where exercise part two complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -12,3 +12,26 @@ WHERE last_name LIKE ('E%');
 SELECT *
 FROM employees
 WHERE last_name LIKE ('%q%');
+
+#Part Two
+SELECT *
+FROM employees
+WHERE gender = 'M'
+    AND (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT *
+FROM employees
+WHERE last_name like '%q%'
+    AND last_name NOT LIKE '%qu%';
+
