00b0e67294d62df73a0778bf5814efe4b44174d5
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-19 09:19:42 -0500

    chore: complete Basic Statments Exercise #2-3

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,15 @@
 USE codeup_test_db;
-
+#2-3
 SELECT * From albums WHERE artist = 'Pink Floyd';
 
 SELECT release_date FROM albums Where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+
+SELECT name FROM albums where sales < 20000000;
+
+SELECT name FROM albums where genre = 'Rock';
+
+
