3aac8bc43868e86e2e765a82c0bcb326a9667e9a
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-19 15:07:00 -0500

    chore: Where Exercise Part One complete

---
+++ where_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+#Part One
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%q%');
