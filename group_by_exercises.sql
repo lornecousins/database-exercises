15fe9baaf1aa22c0cbca4887259f6002a4f1d789
Lorne Cousins <lorne.cousins88@gmail.com>
2022-04-20 13:58:01 -0500

    group by exercise walkthru

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,29 +4,47 @@ SELECT DISTINCT title
 FROM titles;
 
 #3
-SELECT DISTINCT last_name
+SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY last_name, first_name;
+GROUP BY last_name
+ORDER BY last_name;
 
 #4
 SELECT DISTINCT last_name, first_name
 FROM employees
 WHERE last_name Like 'e%e';
+# walkthru:
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name;
 
 #5
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name like '%q%'
   AND last_name NOT LIKE '%qu%';
+# walkthru:
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name like '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
 
 #6
-SELECT DISTINCT last_name, COUNT(*)
+SELECT last_name, COUNT(*)
 FROM employees
 WHERE last_name like '%q%'
   AND last_name NOT LIKE '%qu%'
 GROUP BY last_name
 ORDER BY last_name;
+# walkthru:
+SELECT COUNT(*), last_name
+FROM employees
+WHERE last_name like '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
 
 #7
 SELECT COUNT(*), gender
